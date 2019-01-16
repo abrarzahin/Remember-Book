f42d3cebc6f5b0bc7c19b9892c0da3abb47b8534
abrar zahin <abrarzahin@yahoo.com>
2019-01-15 19:32:40 -0500

    2 SQLite added

--- app/src/main/java/com/example/abrarzahin/rememberbook/MainActivity.java
+++ app/src/main/java/com/example/abrarzahin/rememberbook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.abrarzahin.rememberbook;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DBOpenHelper helper = new DBOpenHelper(this);
+        SQLiteDatabase database= helper.getWritableDatabase();
     }
+
+
+
 }
