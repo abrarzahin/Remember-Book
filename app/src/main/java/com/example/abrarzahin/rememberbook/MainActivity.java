940142bddb731207cb3db03d2ca8d1699d05a7b2
abrar zahin <abrarzahin@yahoo.com>
2019-01-16 19:21:12 -0500

    providers

--- app/src/main/java/com/example/abrarzahin/rememberbook/MainActivity.java
+++ app/src/main/java/com/example/abrarzahin/rememberbook/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DBOpenHelper helper = new DBOpenHelper(this);
-        SQLiteDatabase database= helper.getWritableDatabase();
+
     }
 
 
