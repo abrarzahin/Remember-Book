7d68a7192cb9f2310dc758264c7eac392f75cac7
abrar zahin <abrarzahin@yahoo.com>
2019-01-21 19:00:04 -0500

    add and del

--- app/src/main/java/com/example/abrarzahin/rememberbook/MainActivity.java
+++ app/src/main/java/com/example/abrarzahin/rememberbook/MainActivity.java
@@ -1,39 +1,120 @@
 package com.example.abrarzahin.rememberbook;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.CursorAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+public class MainActivity extends AppCompatActivity
+    implements LoaderManager.LoaderCallbacks<Cursor> {
+     private CursorAdapter cursorAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
-        insertNote("New note");
-
-        Cursor cursor= getContentResolver().query(NotesProvider.CONTENT_URI,
-                DBOpenHelper.ALL_COLUMNS,null,null,null,null);
         String [] from= {DBOpenHelper.NOTE_TEXT};
         int [] to= {android.R.id.text1};
-        CursorAdapter cursorAdapter= new SimpleCursorAdapter(this,
-                android.R.layout.simple_list_item_1,cursor,from,to,0);
+        cursorAdapter = new SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,null,from,to,0);
         ListView list= (ListView) findViewById(android.R.id.list);
         list.setAdapter(cursorAdapter);
 
+        getSupportLoaderManager().initLoader(0,null,this);
+
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.action_create_sample:
+                insertSampleData();
+                break;
+            case R.id.action_delete_all:
+                deleteAllNotes();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private void deleteAllNotes() {
+        DialogInterface.OnClickListener dialogClickListener =
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int button) {
+                        if (button == DialogInterface.BUTTON_POSITIVE) {
+
+                            getContentResolver().delete(
+                                    NotesProvider.CONTENT_URI, null, null
+                            );
+                            restartLoader();
+
+                            Toast.makeText(MainActivity.this,
+                                    getString(R.string.all_deleted),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.are_you_sure))
+                .setPositiveButton(getString(android.R.string.yes), dialogClickListener)
+                .setNegativeButton(getString(android.R.string.no), dialogClickListener)
+                .show();
+
+    }
+
+    private void insertSampleData() {
+        insertNote("\n");
+        insertNote("Simple note");
+        insertNote("Multi-line\nnote");
+        insertNote("Very long note with a lot of text that exceeds the width of the screen");
+        restartLoader();
+    }
+
+    private void restartLoader() {
+        getSupportLoaderManager().restartLoader(0, null, this);
+    }
+
 
     private void insertNote(String noteText) {
         ContentValues values= new ContentValues();
@@ -43,4 +124,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(this,NotesProvider.CONTENT_URI,
+                null,null,null,null);
+    }
+
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        cursorAdapter.swapCursor(data);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        cursorAdapter.swapCursor(null);
+
+    }
 }
